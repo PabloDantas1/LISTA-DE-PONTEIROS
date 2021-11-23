870fbb05b3cdff4e6255487501d4b20391d9b95e
PabloDantas1 <94858698+PabloDantas1@users.noreply.github.com>
2021-11-23 18:14:39 -0300

    Update and rename resolução to resolução.c

---
+++ questao09/resolução.c
@@ -0,0 +1,19 @@
+int main(){
+  int vet[] = {4,9,13};
+  int i;
+  for(i=0;i<3;i++){
+  printf("%d ",*(vet+i));
+  }
+
+// O código irá exibir todos os valores contidos nas diferentes posições do vetor.
+
+}
+int main(){
+  int vet[] = {4,9,13};
+  int i;
+  for(i=0;i<3;i++){
+  printf("%X ",vet+i);
+  }
+}
+
+// O código está a exibir a posição da memória em cada valor citado.
