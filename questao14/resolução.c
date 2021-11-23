145a22c3d2804105882bde174b49556c6305b8bc
PabloDantas1 <94858698+PabloDantas1@users.noreply.github.com>
2021-11-23 18:35:25 -0300

    Update resolução.c

--- questao14/resolução.c
+++ questao14/resolução.c
@@ -1,28 +1,28 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include <stdio.h> // Incluir novas funções como scanf
+#include <stdlib.h> // Incluir novas funções como qsort e malloc
 
-int comp (const void * a, const void * b)
+int comp (const void * a, const void * b) // Comparar váriavel com tipo float
 {
   if ( *(float*)a <  *(float*)b ) return -1;
   if ( *(float*)a == *(float*)b ) return 0;
   if ( *(float*)a >  *(float*)b ) return 1;
 }
 
-int main(void) {
-  int n, i, j;
-  float *vetor, aux;
-  printf("Quantos valores você quer ordenar? ");
-  scanf("%d", &n);
+int main(void) { // Inicio do programa
+  int n, i, j; // Variaveís declaradas
+  float *vetor, aux; // Ponteiro float
+  printf("Quantos valores você quer ordenar? "); // Interação na tela para responder
+  scanf("%d", &n); // Números de sequências
 
-  vetor = malloc(n*sizeof(int));
+  vetor = malloc(n*sizeof(int)); // Utilização da função malloc
 
-  for (i=0;i<n;i++){
-    printf("Digite o valor %d: ", i+1);
-    scanf("%f", &vetor[i]);
+  for (i=0;i<n;i++){ // Iniciação do laço com os valores do vetor
+    printf("Digite o valor %d: ", i+1); // Interação na tela para responder
+    scanf("%f", &vetor[i]); // Utilização da função scanf
   }
 
-  qsort (vetor, n, sizeof(int), comp);
-  for (i=0; i<n; i++)
+  qsort (vetor, n, sizeof(int), comp); // Utilização da função qsort
+  for (i=0; i<n; i++) // Resultados impressos na tela
      printf ("%f ",vetor[i]);
   return 0;
 }
