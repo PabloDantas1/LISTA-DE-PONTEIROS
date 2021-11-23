95508126790789cc18b18a7742bc1b3ab4233e7d
PabloDantas1 <94858698+PabloDantas1@users.noreply.github.com>
2021-11-23 18:05:18 -0300

    Update resolução.c

--- questao04/resolução.c
+++ questao04/resolução.c
@@ -3,12 +3,12 @@
 int main(void) {
   int i, j, *p, *q;
   p = &i;
-  *q = &j;
+  *q = &j; // Ilegal
   p = &*&i;
   i = (*&)j; // Ilegal
   i = *&j;
   i = &&j; // Ilegal
   q = *p;
-  i = (*p) + *q;
+  i = (*p) + *q; // Ilegal
   return 0;
 }
